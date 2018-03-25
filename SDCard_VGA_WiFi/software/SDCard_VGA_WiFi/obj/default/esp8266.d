936c7d29d44481df09bf918a12310ef133a178ed
M10512001 <32129370+M10512001@users.noreply.github.com>
2018-03-25 14:57:12 +0800

    With RFS Daughter Card
    Quartus Prime 15.1, Verilog HDL, Qsys, Nios II, C

---
+++ SDCard_VGA_WiFi/software/SDCard_VGA_WiFi/obj/default/esp8266.d
@@ -0,0 +1,20 @@
+obj/default/esp8266.o: esp8266.c ../SDCard_VGA_WiFi_bsp/system.h \
+ ../SDCard_VGA_WiFi_bsp/linker.h esp8266.h \
+ ../SDCard_VGA_WiFi_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../SDCard_VGA_WiFi_bsp//HAL/inc/io.h \
+ ../SDCard_VGA_WiFi_bsp//HAL/inc/alt_types.h \
+ ../SDCard_VGA_WiFi_bsp//drivers/inc/altera_avalon_uart_regs.h
+
+../SDCard_VGA_WiFi_bsp/system.h:
+
+../SDCard_VGA_WiFi_bsp/linker.h:
+
+esp8266.h:
+
+../SDCard_VGA_WiFi_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../SDCard_VGA_WiFi_bsp//HAL/inc/io.h:
+
+../SDCard_VGA_WiFi_bsp//HAL/inc/alt_types.h:
+
+../SDCard_VGA_WiFi_bsp//drivers/inc/altera_avalon_uart_regs.h:
---
+++ SDCard_VGA_WiFi/software/SDCard_VGA_WiFi/obj/default/main.d
@@ -0,0 +1,33 @@
+obj/default/main.o: main.c ../SDCard_VGA_WiFi_bsp/system.h \
+ ../SDCard_VGA_WiFi_bsp/linker.h \
+ ../SDCard_VGA_WiFi_bsp//drivers/inc/Altera_UP_SD_Card_Avalon_Interface.h \
+ ../SDCard_VGA_WiFi_bsp//HAL/inc/alt_types.h \
+ ../SDCard_VGA_WiFi_bsp//HAL/inc/sys/alt_dev.h \
+ ../SDCard_VGA_WiFi_bsp//HAL/inc/sys/alt_llist.h \
+ ../SDCard_VGA_WiFi_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../SDCard_VGA_WiFi_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+ ../SDCard_VGA_WiFi_bsp//drivers/inc/altera_avalon_uart_regs.h \
+ ../SDCard_VGA_WiFi_bsp//HAL/inc/io.h \
+ ../SDCard_VGA_WiFi_bsp//HAL/inc/alt_types.h
+
+../SDCard_VGA_WiFi_bsp/system.h:
+
+../SDCard_VGA_WiFi_bsp/linker.h:
+
+../SDCard_VGA_WiFi_bsp//drivers/inc/Altera_UP_SD_Card_Avalon_Interface.h:
+
+../SDCard_VGA_WiFi_bsp//HAL/inc/alt_types.h:
+
+../SDCard_VGA_WiFi_bsp//HAL/inc/sys/alt_dev.h:
+
+../SDCard_VGA_WiFi_bsp//HAL/inc/sys/alt_llist.h:
+
+../SDCard_VGA_WiFi_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../SDCard_VGA_WiFi_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
+../SDCard_VGA_WiFi_bsp//drivers/inc/altera_avalon_uart_regs.h:
+
+../SDCard_VGA_WiFi_bsp//HAL/inc/io.h:
+
+../SDCard_VGA_WiFi_bsp//HAL/inc/alt_types.h:
